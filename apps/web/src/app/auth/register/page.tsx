97a51fb509fd92c61461e13fe0090596495c120e
cryptedoji <ojiinnocent@gmail.com>
2026-06-22 13:52:33 +0100

    remove: delete web folder  from remote

--- apps/web/src/app/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "@/components/auth/RegisterForm";
-
-export default function RegisterPage() {
-  return (
-    <div className="min-h-[80vh] flex items-center justify-center bg-gray-50 px-4">
-      <RegisterForm />
-    </div>
-  );
-}
